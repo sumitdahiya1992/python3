620a4104ed9e03f0cb2a7cf48ae5d74b93020add
sumitdahiya1992 <sumit37367@gmail.com>
2024-01-30 10:16:01 +0530

    this is 3 program

---
+++ pro3.py
@@ -0,0 +1,15 @@
+# write a python program to find the area of a triangle
+
+# input the base and height from the user
+
+base = float(input("enter the length of the base of the triangle: "))
+height = float(input("enter the heiht of the triangle: "))
+
+# calculate the area of the triangle
+
+area = 0.5 * base * height
+
+# display the result
+
+print(f"the area of the triangle is: {area}")
+
